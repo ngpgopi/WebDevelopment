2143ecb336addf8ff7eb43ed974fbe5733d7d1a7
Gopi Krishnan <31714484+ngpgopi@users.noreply.github.com>
2018-03-30 17:54:21 -0500

    Add files via upload

---
+++ Portfoilo - Mywebsite/portfolio.js
@@ -0,0 +1,25 @@
+
+
+var he1 = document.querySelectorAll(".he1");
+var arr = [
+	"https://github.com/ngpgopi/MiniCompilerDesign",
+	"https://github.com/ngpgopi/MutationTesting",
+	"https://github.com/ngpgopi/WebDevelopment/tree/master/HTMLColorGameProject"
+];
+
+
+he1[0].addEventListener("click" ,function (){
+	window.open("https://github.com/ngpgopi/MiniCompilerDesign", '_blank');
+});
+
+
+he1[1].addEventListener("click" ,function (){
+	window.open("https://github.com/ngpgopi/MutationTesting", '_blank');
+});
+
+he1[2].addEventListener("click" ,function (){
+	window.open("https://github.com/ngpgopi/WebDevelopment/tree/master/HTMLColorGameProject", '_blank');
+});
+
+
+
